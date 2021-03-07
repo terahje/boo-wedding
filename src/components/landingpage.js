635f450343851e89d74a82a964bdd454a253517d
terahje <terahjequinn@gmail.com>
2021-03-07 00:01:36 -0700

    structure started for landing page. added countdown for days till wedding

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,16 +1,64 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import FlipCountdown from '@rumess/react-flip-countdown';
 
 class Landing extends Component {
     render() {
         return(
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className="landing-grid">
-                    <Cell col={12}>
-                    
-                    </Cell>
-                </Grid>
-            </div>
+            <div >
+            <Grid className="">
+                <Cell col={4}>
+                <FlipCountdown
+                  hideYear
+                  hideHour
+                  hideMinute
+                  hideSecond
+                size='medium' // Options (Default: medium): large, medium, small, extra-small.
+                className="countdown"
+                endAt={'2021-09-25 17:00:00'} // Date/Time
+            />
+                </Cell>
+                <Cell col={4}>
+                 
+                </Cell>
+                <Cell col={4}>
+                 
+                </Cell>
+            </Grid>
+            <Grid className="">
+                <Cell col={4}>
+                 
+                </Cell>
+                <Cell col={4}>
+                 
+                </Cell>
+                <Cell col={4}>
+                 
+                </Cell>
+            </Grid>
+            <Grid className="landing-grid">
+                <Cell col={1}>
+                 
+                </Cell>
+                <Cell col={10}>
+                <div className="banner-text" style={{ textAlign: 'center' }}>
+                    <h3 className="wedDate" style={{ fontFamily: 'Martel, serif'}}>
+                        09.25.21
+                        </h3>
+                    <h1>Ruve & Terahje</h1>
+                    <h3 style={{ fontFamily: 'Martel, serif'}}>
+                    Are Getting Married!
+
+                    </h3>
+
+                    {/* <hr/> */}
+                </div>
+                </Cell>
+                <Cell col={1}>
+                 
+                </Cell>
+            </Grid>
+        </div>
         )
     }
 }
