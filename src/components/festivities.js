deec45b7c83ce835c9104d5f1f1da1b942728be0
terahje <terahjequinn@gmail.com>
2021-03-21 17:52:10 -0700

    updated festivities page

---
+++ src/components/festivities.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+// import ourPic from '../assets/images/R&Tovershoulder.jpg'
+
+const festivities = () => {
+    return (
+        <section id="festPage">
+            <div className="backBtn">
+                <Link to="/">
+                ← Back to Home
+                </Link>
+             </div>
+
+            <div className="festContainer">
+                <div className="festMain">
+                    <h1>Ruve & Terahje's Wedding</h1>
+                    <h4>September 25, 2021</h4>
+                    <h4>2649 E. 6th St. <br/>
+                        Tucson, AZ 85716</h4>
+                    <div className="festFull">
+                        <div className="festTimeLeft">
+                            <h3>5:00 PM</h3>
+                            <h3>5:30 PM</h3>
+                            <h3>6:00 PM</h3>
+                            <h3>7:00 PM</h3>
+                        </div>
+                        <div className="festDescRight">
+                            <h3>Cermony</h3>
+                            <h3>Cocktail Hour</h3>
+                            <h3>Buffet Dinner</h3>
+                            <h3>Dancing,<br/>Photobooth,<br/>Yard Games</h3>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    )
+}
+
+export default festivities
--- src/components/main.js
+++ src/components/main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import LandingPage from './landingpage';
 import OurStory from './ourStory';
 import Registry from './registry';
+import Festivities from './festivities';
 // import Contact from './contact';
 // import Projects from './projects';
 // import Resume from './resume';
@@ -13,6 +14,7 @@ const Main = () => (
         <Route exact path= "/" component={ LandingPage } />
         <Route path= "/ourStory" component={ OurStory } />
         <Route path= "/registry" component={ Registry } />
+        <Route path= "/festivities" component={ Festivities } />
         {/* <Route path= "/contact" component={ Contact } /> */}
         {/* <Route path= "/projects" component={ Projects } /> */}
         {/* <Route path= "/resume" component={ Resume } /> */}
