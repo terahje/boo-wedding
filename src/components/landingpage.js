2b59524ca890203802df62f4a0647dcd48ac8352
terahje <terahjequinn@gmail.com>
2021-03-23 14:23:21 -0700

    refactored landing page, previous method was used react mdl, but method to achieve format was forced

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -5,63 +5,38 @@ import FlipCountdown from '@rumess/react-flip-countdown';
 class Landing extends Component {
     render() {
         return(
-            <section id="landing">
+            <section id="landing" >
+                <section id="landingFull">
+                    <div className="flipCount">
+                        <FlipCountdown
+                        hideYear
+                        hideHour
+                        hideMinute
+                        hideSecond
+                        size='medium' // Options (Default: medium): large, medium, small, extra-small.
+                        className="countdown"
+                        endAt={'2021-09-25 17:00:00'} // Date/Time
+                    />
+                    </div>
+                    <section id="landingCon">
+                            <div className="landingMain">
+                                <h3 className="wedDate">
+                                    09.25.21
+                                    </h3>
+                                <h1>Ruve & Terahje</h1>
+                                <h3>
+                                Are Getting Married!
 
-            <Grid className="">
-                <Cell col={4}>
-                <FlipCountdown
-                  hideYear
-                  hideHour
-                  hideMinute
-                  hideSecond
-                size='medium' // Options (Default: medium): large, medium, small, extra-small.
-                className="countdown"
-                endAt={'2021-09-25 17:00:00'} // Date/Time
-            />
-                </Cell>
-                <Cell col={4}>
-                 
-                </Cell>
-                <Cell col={4}>
-                 
-                </Cell>
-            </Grid>
-            <Grid className="">
-                <Cell col={4}>
-                 
-                </Cell>
-                <Cell col={4}>
-                 
-                </Cell>
-                <Cell col={4}>
-                 
-                </Cell>
-            </Grid>
-            <Grid className="landing-grid">
-                <Cell col={1}>
-                 
-                </Cell>
-                <Cell col={10}>
-                <div className="banner-text" style={{ textAlign: 'center' }}>
-                    <h3 className="wedDate" style={{ fontFamily: 'Martel, serif'}}>
-                        09.25.21
-                        </h3>
-                    <h1>Ruve & Terahje</h1>
-                    <h3 style={{ fontFamily: 'Martel, serif'}}>
-                    Are Getting Married!
+                                </h3>
 
-                    </h3>
+                                {/* <hr/> */}
+                            </div>
+                        
+                    </section>
+                
+                </section>
 
-                    {/* <hr/> */}
-                </div>
-                </Cell>
-                <Cell col={1}>
-                 
-                </Cell>
-            </Grid>
-            <div className="footerSpacer">
-            </div>
-        </section>
+            </section>
        
         )
     }
