833ce964a1d197ce3117b95c3a1074938fad1da0
terahje <terahjequinn@gmail.com>
2021-03-28 18:26:35 -0700

    getting set up to make site responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './media-queries.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-mdl/extra/material.css';
