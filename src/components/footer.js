389e71f643b98ad458c66c4287525c5bec0f6412
terahje <terahjequinn@gmail.com>
2021-04-27 11:44:40 -0700

    learned new media technique to add media queries with landscape orientation.

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,8 @@ const footer = () => {
     //    Footer
 
         <footer id="Footer">
-           <p>All Rights Reserved <span>&copy;</span><b>Terahje</b> 2021</p>
+           <p>All Rights Reserved <span>&copy;</span><b><a href="https://github.com/terahje">
+               Terahje</a></b> 2021</p>
        </footer>
 
 // End of Footer 
